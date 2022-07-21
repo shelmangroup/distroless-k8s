1c6ac7fab5f6dc84fe133723dd76298fa76c1e9f
Daniel Lundin <daniel@arity.se>
2022-07-21 14:15:52 +0200

    fix broken build-local script

--- build-local.sh
+++ build-local.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
+
+ARCH=$(uname -m)
 set -feu -o pipefail
+
+ARCH=$(uname -m)
 
 _melange() {
   podman run --rm -it --privileged -v $(pwd):/github/workspace -w /github/workspace ghcr.io/distroless/melange "$@"
@@ -9,12 +13,10 @@ _melange() {
 test -f melange.rsa || _melange keygen
 
 # Build package
-_melange build melange.yaml --arch arm64 -k melange.rsa
-
-exit 0
+_melange build melange.yaml --arch "$ARCH" -k melange.rsa
 
 # Generate repo index
-podman run --rm -it -v $(pwd):/w -w /w/packages -v $(pwd)/packages:/w/packages \
+podman run --rm -it -v "$(pwd):/w" -w /w/packages -v "$(pwd)/packages:/w/packages" \
     --entrypoint sh \
     ghcr.io/distroless/melange -c \
         'for d in `find . -type d -mindepth 1`; do \
@@ -27,11 +29,12 @@ podman run --rm -it -v $(pwd):/w -w /w/packages -v $(pwd)/packages:/w/packages \
 
 # Build image with apko
 podman run --rm -it \
-    -v $(pwd):/github/workspace -w /github/workspace \
-    -v $(pwd)/packages:/github/workspace/packages \
+    -v "$(pwd):/github/workspace" \
+    -v "$(pwd)/packages:/github/workspace/packages" \
+    -w /github/workspace \
     ghcr.io/distroless/apko build --debug apko.yaml \
-    test output.tar -k melange.rsa.pub \
-    --build-arch arm64
+      distroless-k8s-dev output.tar -k melange.rsa.pub \
+      --build-arch "$ARCH"
 
 # Load image
 podman load -i output.tar
