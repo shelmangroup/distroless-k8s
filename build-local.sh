3a7d85cf5b858897e79df2ba6b8d24b9bc67b94b
Daniel Lundin <daniel@arity.se>
2022-07-23 14:55:37 +0200

    format

--- build-local.sh
+++ build-local.sh
@@ -6,7 +6,7 @@ set -feu -o pipefail
 ARCH=$(uname -m)
 
 _melange() {
-  podman run --rm -it --privileged -v $(pwd):/github/workspace -w /github/workspace ghcr.io/distroless/melange "$@"
+	podman run --rm -it --privileged -v "$(pwd):/github/workspace" -w /github/workspace ghcr.io/distroless/melange "$@"
 }
 
 # Get binaries
@@ -21,25 +21,25 @@ _melange build melange.yaml --arch "$ARCH" -k melange.rsa
 
 # Generate repo index
 podman run --rm -it -v "$(pwd):/w" -w /w/packages -v "$(pwd)/packages:/w/packages" \
-    --entrypoint sh \
-    ghcr.io/distroless/melange -c \
-        'for d in `find . -type d -mindepth 1`; do \
-            ( \
-                cd $d && \
-                apk index -o APKINDEX.tar.gz *.apk && \
-                melange sign-index --signing-key=../../melange.rsa APKINDEX.tar.gz\
-            ) \
-        done'
+	--entrypoint sh \
+	ghcr.io/distroless/melange -c \
+	'for d in `find . -type d -mindepth 1`; do \
+    ( \
+      cd $d && \
+      apk index -o APKINDEX.tar.gz *.apk && \
+      melange sign-index --signing-key=../../melange.rsa APKINDEX.tar.gz\
+    ) \
+   done'
 
 # Build image with apko
 podman run --rm -it \
-    -v "$(pwd):/github/workspace" \
-    -v "$(pwd)/packages:/github/workspace/packages" \
-    -w /github/workspace \
-    -e SOURCE_DATE_EPOCH=0 \
-    ghcr.io/distroless/apko build --debug apko.yaml \
-      distroless-k8s-dev output.tar -k melange.rsa.pub \
-      --build-arch "$ARCH"
+	-v "$(pwd):/github/workspace" \
+	-v "$(pwd)/packages:/github/workspace/packages" \
+	-w /github/workspace \
+	-e SOURCE_DATE_EPOCH=0 \
+	ghcr.io/distroless/apko build --debug apko.yaml \
+	distroless-k8s-dev output.tar -k melange.rsa.pub \
+	--build-arch "$ARCH"
 
 # Load image
 podman load -i output.tar
