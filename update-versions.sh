25a4f38117d95d739c5c368be0323369025340d8
Daniel Lundin <daniel@arity.se>
2023-01-18 09:43:40 +0100

    Automatically update version env vars in apko spec

--- update-versions.sh
+++ update-versions.sh
@@ -6,8 +6,8 @@ KAPP_VERSION=$(curl -fsL https://api.github.com/repos/vmware-tanzu/carvel-kapp/r
 KBLD_VERSION=$(curl -fsL https://api.github.com/repos/vmware-tanzu/carvel-kbld/releases/latest | jq -r '.tag_name')
 KUBECTL_VERSION=$(curl -fsL https://dl.k8s.io/release/stable.txt)
 
-kustomize_release=$(curl -s https://api.github.com/repos/kubernetes-sigs/kustomize/releases | /usr/bin/jq -r '.[].tag_name | select(contains("kustomize"))' \
-  | sort -rV | head -n 1)
+kustomize_release=$(curl -s https://api.github.com/repos/kubernetes-sigs/kustomize/releases | /usr/bin/jq -r '.[].tag_name | select(contains("kustomize"))' |
+	sort -rV | head -n 1)
 KUSTOMIZE_VERSION=$(basename "${kustomize_release}")
 
 here=$(dirname "$(readlink -f "$0")")
@@ -25,13 +25,12 @@ KUBECTL_VERSION=${KUBECTL_VERSION}
 KUSTOMIZE_VERSION=${KUSTOMIZE_VERSION}
 EOF
 
-## FIXME: leaving these out until https://github.com/chainguard-dev/apko/issues/301 is fixed
-# apko_yaml="${here}/apko.yaml"
-# yq -Y -i ".environment.HELM_VERSION = \"${HELM_VERSION}\"" "${apko_yaml}"
-# yq -Y -i ".environment.KAPP_VERSION = \"${KAPP_VERSION}\"" "${apko_yaml}"
-# yq -Y -i ".environment.KBLD_VERSION = \"${KBLD_VERSION}\"" "${apko_yaml}"
-# yq -Y -i ".environment.KUBECTL_VERSION = \"${KUBECTL_VERSION}\"" "${apko_yaml}"
-# yq -Y -i ".environment.KUSTOMIZE_VERSION = \"${KUSTOMIZE_VERSION}\"" "${apko_yaml}"
+apko_yaml="${here}/apko.yaml"
+yq -Y -i ".environment.HELM_VERSION = \"${HELM_VERSION}\"" "${apko_yaml}"
+yq -Y -i ".environment.KAPP_VERSION = \"${KAPP_VERSION}\"" "${apko_yaml}"
+yq -Y -i ".environment.KBLD_VERSION = \"${KBLD_VERSION}\"" "${apko_yaml}"
+yq -Y -i ".environment.KUBECTL_VERSION = \"${KUBECTL_VERSION}\"" "${apko_yaml}"
+yq -Y -i ".environment.KUSTOMIZE_VERSION = \"${KUSTOMIZE_VERSION}\"" "${apko_yaml}"
 
 melange_yaml="${here}/melange.yaml"
 yq -Y -i ".package.version = \"${KUBECTL_SEMVER}\"" "${melange_yaml}"
